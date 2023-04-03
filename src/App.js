12e3c2f162f39c17df01808a4ba0bcad5016453e
Andrian Raihannudin <reedbulls91@gmail.com>
2023-04-03 17:24:59 +0700

    cloning weverse.shop : cra & fetch API Artist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [searchFields , setSearchFields] = useState('')
+  const [ monsters, setMonster ] = useState([])
+  const [title , setTitle] = useState('Artist')
+
+  useEffect(() =>{
+    // get data from API
+    fetch("http://127.0.0.1:8000/api/artist")
+        //     // make respionse to jsonn
+        .then((response) => response.json())
+        .then((monster => setMonster(monster)));
+  }, [])
+
+  console.log(monsters);
+
+
+  return(
+      <div className="App">
+        <h1 className="app-title">{title}</h1>
+        {monsters.map((monster) => {
+          return(
+              <>
+                <p key={monster.id}>{monster.name}</p>
+              </>
+          )
+        })}
+      </div>
+  )
 }
+
 
 export default App;
