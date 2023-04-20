0ad20c38d0d8ca52459851a76fd2b6c2ca2d44f7
Andrian Raihannudin <reedbulls91@gmail.com>
2023-04-20 09:58:11 +0700

    cloning weverse.shop.copy : order

--- src/components/Card/OrderCard.Component.jsx
+++ src/components/Card/OrderCard.Component.jsx
@@ -1,62 +1,80 @@
+import React from "react";
 
 export const OrderCardComponent = () => {
     return(
         <>
-            <div>
-                <div className="flex justify-between" style={{ borderBottom:"1px solid #ebebeb"}}>
-                    <div className="flex my-3">
-                        <div className="me-3 my-auto">
-                            <input className="my-7" style={{ width:"22px" , fontSize:"30px" }} type="radio"/>
+            <div className=" bg-gray-50 my-6  p-4 rounded-lg  dark:bg-gray-800">
+                <div className="mx-4">
+                    <div className="flex  justify-between">
+                        <div>
+                            <p className="text-sm text-gray-500 dark:text-gray-400">
+                                <div className="text-left">
+                                    <p className="font-medium mx-4 cursor-pointer " style={{ fontSize:"15px" , borderBottom:"1px solid #ebebebe"}}>BTS</p>
+                                </div>
+                            </p>
                         </div>
-                        <div className="my-2" style={{ width:"60px" , height:"60px"}}>
-                            <img className="w-full h-full"
-                                 src="https://cdn-contents.weverseshop.io/public/shop/c6e60dbc87d9db215bed26652ee71f41.jpg?q=95&w=720"
-                                 alt="BTS"/>
-                        </div>
-                        <div className="text-left mx-3 my-2">
-                            <h5 style={{ fontSize:"16px"}}>LOVE YOURSELF 轉 'Tear'</h5>
-                            <p style={{ fontSize:"15px", color:"#a6a6a6"}}>Options : LOVE YOURSELF 轉 'Tear' Version: O</p>
-                        </div>
-                    </div>
-                    <div className="flex ms-0">
-                        <div className="my-auto mx-6">
-                            <div className="flex gap-1">
-                                <h2 className="font-normal my-auto" style={{ fontSize:"16px"}}>Quantity:</h2>
-                                <h2 className="font-normal my-auto" style={{ color:"#35c3c4" ,fontSize:"16px"}}>1</h2>
+                        <div className="w-4/12 my-auto ">
+                            <div className="my-1 text-left flex">
+                                <p className="font-semibold my-auto text-left mx-3" style={{ color:"#3ECCCD"}}>Status:</p>
+                                <div className="flex my-1 w-full justify-between" style={{ color:"rgba(131,130,130,0.87)"}} >
+                                    <div className="text-left flex gap-2">
+                                        <i className="fa-solid fa-box my-auto"></i>
+                                        <p >16-04-2023 12:06</p>
+                                    </div>
+                                    <div className="text-right">
+                                        <p className="">Pesanan Dibuat</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+                    </div>
+                    <div className="flex justify-between " style={{ borderBottom:"1px solid #ebebeb"}}>
+                        <div className="flex my-3">
+                            <div className="me-3 my-auto">
+                                <input className="my-7" style={{ width:"22px" , fontSize:"30px" }} type="radio"/>
+                            </div>
+                            <div className="my-2" style={{ width:"60px" , height:"60px"}}>
+                                <img className="w-full h-full"
+                                     src="https://cdn-contents.weverseshop.io/public/shop/c6e60dbc87d9db215bed26652ee71f41.jpg?q=95&w=720"
+                                     alt="BTS"/>
+                            </div>
+                            <div className="text-left mx-3 my-2">
+                                <h5 style={{ fontSize:"16px"}}>LOVE YOURSELF 轉 'Tear'</h5>
+                                <p style={{ fontSize:"15px", color:"#a6a6a6"}}>Options : LOVE YOURSELF 轉 'Tear' Version: O</p>
+                            </div>
+                        </div>
+                        <div className="flex ms-0">
+                            <div className="my-auto mx-6">
+                                <div className="flex gap-1">
+                                    <h2 className="font-normal my-auto" style={{ fontSize:"16px"}}>Quantity:</h2>
+                                    <h2 className="font-normal my-auto" style={{ color:"#35c3c4" ,fontSize:"16px"}}>1</h2>
+                                </div>
+                            </div>
                             <div className="my-auto mx-10">
                                 <div className="flex gap-1">
                                     <h2 className="font-normal my-auto" style={{ fontSize:"16px"}}>Status:</h2>
                                     <h2 className="font-normal my-auto" style={{ color:"#35c3c4" ,fontSize:"16px"}}>Packing</h2>
                                 </div>
                             </div>
-                        <div className="my-auto mx-0">
-                            <div className="flex gap-1">
-                                <h2 className="font-normal my-auto" style={{ fontSize:"16px"}}>Price:</h2>
-                                <h2 className="font-semibold my-auto" style={{ color:"#35c3c4" ,fontSize:"16px"}}>$20</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex justify-between">
-                    <div className="w-5/12 my-auto ">
-                        <div className="my-1 text-left flex">
-                            <p className="font-semibold my-auto text-left mx-3" style={{ color:"#3ECCCD"}}>Status:</p>
-                            <div className="flex my-1 w-full justify-between" style={{ color:"rgba(131,130,130,0.87)"}} >
-                                <div className="text-left flex gap-2">
-                                    <i className="fa-solid fa-box my-auto"></i>
-                                    <p >16-04-2023 12:06</p>
-                                </div>
-                                <div className="text-right">
-                                    <p className="">Pesanan Dibuat</p>
+                            <div className="my-auto mx-0">
+                                <div className="flex gap-1">
+                                    <h2 className="font-normal my-auto" style={{ fontSize:"16px"}}>Price:</h2>
+                                    <h2 className="font-semibold my-auto" style={{ color:"#35c3c4" ,fontSize:"16px"}}>$20</h2>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="flex w-4/12 gap-3">
-                        <button className="w-full my-4 font-medium py-2 px-6 " style={{ color:"#40CDCC" , borderRadius:"4px" , fontSize:"14px" , border:"1px solid #40CDCC"}}>Order Receive</button>
-                        <button className="w-full my-4 font-medium py-2 px-6 " style={{ color:"#40CDCC" , borderRadius:"4px" , fontSize:"14px" , border:"1px solid #40CDCC"}}>Order Detail</button>
+                    <div className="flex justify-between">
+                        <div className="w-5/12 my-auto">
+                            <div className=" flex gap-2 mx-6">
+                                <h2 className="my-auto" style={{ fontSize:"20px"}}>Total Order:</h2>
+                                <h2 className="font-semibold" style={{ fontSize:"20px"}}>$40</h2>
+                            </div>
+                        </div>
+                        <div className="flex w-4/12 gap-3">
+                            <button className="w-full my-4 font-medium py-2 px-6 " style={{ color:"#40CDCC" , borderRadius:"4px" , fontSize:"14px" , border:"1px solid #40CDCC"}}>Order Receive</button>
+                            <button className="w-full my-4 font-medium py-2 px-6 " style={{ color:"#40CDCC" , borderRadius:"4px" , fontSize:"14px" , border:"1px solid #40CDCC"}}>Order Detail</button>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/OrderList.Component.jsx
+++ src/components/OrderList.Component.jsx
@@ -1,5 +1,7 @@
 import {CartCardComponent} from "./Card/CartCard.Component";
 import {OrderCardComponent} from "./Card/OrderCard.Component";
+import SupportListCardComponent from "./SupportListCard.Component";
+import React from "react";
 
 
 export const OrderListComponent = () => {
@@ -31,11 +33,116 @@ export const OrderListComponent = () => {
                             {/*    <button className="w-2/12 my-4 font-medium py-3 " style={{ color:"#40CDCC" , borderRadius:"4px" , fontSize:"16px" , border:"1px solid #40CDCC"}}>Shop Now</button>*/}
                             {/*</div>*/}
                             <div className="pb-14">
-                                <ul>
-                                    <li>
-                                        <OrderCardComponent />
-                                    </li>
-                                </ul>
+                                <div className="mb-4 border-b pb-4 border-gray-200 dark:border-gray-700">
+                                    <ul className="flex flex-wrap mx-auto -mb-px text-sm font-medium text-center" id="myTab"
+                                        data-tabs-toggle="#myTabContent" role="tablist">
+                                        <li className80 role="presentation">
+                                            <button
+                                                style={{ width:"180px" , border:"1px solid #ebebeb"}}
+                                                className=" inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700 rounded-l-lg"
+                                                id="all-tab" data-tabs-target="#all" type="button"
+                                                role="tab" aria-controls="profile" aria-selected="false">All
+                                            </button>
+                                        </li>
+                                        <li className="" role="presentation">
+                                            <button
+                                                style={{ width:"180px" , border:"1px solid #ebebeb"}}
+                                                className="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                                                id="shipping-tab" data-tabs-target="#shipping" type="button"
+                                                role="tab" aria-controls="dashboard" aria-selected="false">Packing
+                                            </button>
+                                        </li>
+                                        <li className="" role="presentation">
+                                            <button
+                                                style={{ width:"180px" , border:"1px solid #ebebeb"}}
+                                                className="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                                                id="return-tab" data-tabs-target="#return" type="button" role="tab"
+                                                aria-controls="settings" aria-selected="false">Shipping
+                                            </button>
+                                        </li>
+                                        <li className="" role="presentation">
+                                            <button
+                                                style={{ width:"180px" , border:"1px solid #ebebeb"}}
+                                                className="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                                                id="order-tab" data-tabs-target="#order" type="button" role="tab"
+                                                aria-controls="contacts" aria-selected="false">Successful
+                                            </button>
+                                        </li>
+                                        <li className="" role="presentation">
+                                            <button
+                                                style={{ width:"180px" , border:"1px solid #ebebeb"}}
+                                                className="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                                                id="membership-tab" data-tabs-target="#membership" type="button" role="tab"
+                                                aria-controls="settings" aria-selected="false">Cancel
+                                            </button>
+                                        </li>
+                                        <li className="" role="presentation">
+                                            <button
+                                                style={{ width:"180px" , border:"1px solid #ebebeb"}}
+                                                className="inline-block w-full p-4 bg-white rounded-r-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                                                id="service-tab" data-tabs-target="#service" type="button" role="tab"
+                                                aria-controls="settings" aria-selected="false">Not pay
+                                            </button>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                                <div id="myTabContent">
+                                    <div className="hidden " id="all"
+                                         role="tabpanel" aria-labelledby="all-tab">
+                                        <ul>
+                                            <li>
+                                                <OrderCardComponent />
+                                            </li>
+                                            <li>
+                                                <OrderCardComponent />
+                                            </li>
+                                            <li>
+                                                <OrderCardComponent />
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="hidden " id="shipping"
+                                         role="tabpanel" aria-labelledby="shipping-tab">
+                                        <ul>
+                                            <li>
+                                                <OrderCardComponent />
+                                            </li>
+                                            <li>
+                                                <OrderCardComponent />
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="hidden" id="return"
+                                         role="tabpanel" aria-labelledby="return-tab">
+                                        <ul>
+                                            <li>
+                                                <OrderCardComponent />
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="hidden " id="order"
+                                         role="tabpanel" aria-labelledby="order-tab">
+                                        <div className="pt-36 pb-24">
+                                            <i style={{ fontSize:"50px" , color:"#40CDCC"}} className="fa-solid fa-note-sticky"></i>
+                                            <h2 className="text-center my-4" style={{ color:"#bdbdbd"}}>No Orders</h2>
+                                        </div>
+                                    </div>
+                                    <div className="hidden " id="membership"
+                                         role="tabpanel" aria-labelledby="membership-tab">
+                                        <div className="pt-36 pb-24">
+                                            <i style={{ fontSize:"50px" , color:"#40CDCC"}} className="fa-solid fa-note-sticky"></i>
+                                            <h2 className="text-center my-4" style={{ color:"#bdbdbd"}}>No Orders</h2>
+                                        </div>
+                                    </div>
+                                    <div className="hidden " id="service"
+                                         role="tabpanel" aria-labelledby="service-tab">
+                                        <div className="pt-36 pb-24">
+                                            <i style={{ fontSize:"50px" , color:"#40CDCC"}} className="fa-solid fa-note-sticky"></i>
+                                            <h2 className="text-center my-4" style={{ color:"#bdbdbd"}}>No Orders</h2>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
